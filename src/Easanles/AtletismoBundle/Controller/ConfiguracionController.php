e1899a6147fbab0d09d228e5c1c65f07862b7d99
Eduardo Alonso Sanlés <easanles@gmail.com>
2015-06-10 21:27:31 +0200

    Entidad Competicion

--- src/Easanles/AtletismoBundle/Controller/ConfiguracionController.php
+++ src/Easanles/AtletismoBundle/Controller/ConfiguracionController.php
@@ -28,33 +28,11 @@ class ConfiguracionController extends Controller
     
     public function poblar_bdAction(){
     	$em = $this->getDoctrine()->getManager();
-    	 
-    	$comp = new Competicion();
-        $comp->setNombre("Competición 1")
-             ->setTemporada("2012/2013")
-             ->setTipo("Campo a través")
-             ->setProvincia("A Coruña")
-             ->setEsOficial(true)
-             ->setEsFederada(true);
-    	$em->persist($comp);
-    	
-    	$comp = new Competicion();
-    	$comp->setNombre("Competición 2")
-    	     ->setTemporada("2013/2014")
-    	     ->setTipo("Ruta")
-    	     ->setProvincia("A Coruña")
-    	     ->setEsOficial(true)
-    	     ->setEsFederada(false);
-    	$em->persist($comp);
-    	
-    	$comp = new Competicion();
-    	$comp->setNombre("Competición 3")
-    	     ->setTemporada("2013/2014")
-    	     ->setTipo("Ruta")
-    	     ->setProvincia("Ourense")
-    	     ->setEsOficial(false)
-    	     ->setEsFederada(false);
-    	$em->persist($comp);
+    	    	
+    	$com = new Competicion();
+    	   $com->setNombre("Competicion 1")
+    	       ->setTemp(2015);
+    	$em->persist($com);
     	
     	$em->flush();
     	
--- src/Easanles/AtletismoBundle/Entity/Competicion.php
+++ src/Easanles/AtletismoBundle/Entity/Competicion.php
@@ -6,209 +6,181 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * Competicion
- *
- * @ORM\Table()
+ * 
+ * @ORM\Table(name="com")
  * @ORM\Entity(repositoryClass="Easanles\AtletismoBundle\Entity\CompeticionRepository")
  */
-class Competicion
-{
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
+class Competicion {
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="nombre", type="string", length=255)
-     */
-    private $nombre;
+   /**
+   * @var string
+   * @ORM\Column(name="nombrecom", type="string", length=255)
+   * @ORM\Id
+   */
+   private $nombre;
+   
+   /**
+   * @var integer
+   * @ORM\Column(name="tempcom", type="smallint")
+   * @ORM\Id
+   */
+   private $temp;
+   
+   /**
+   * @var string
+   * @ORM\Column(name="ubicacioncom", type="string", length=255, nullable=true)
+   */
+   private $ubicacion;
+   
+   /**
+   * @var string
+   * @ORM\Column(name="sedecom", type="string", length=255, nullable=true)
+   */
+   private $sede;
+   
+   /**
+   * @ORM\Column(name="fechacom", type="date", nullable=true)
+   */
+   private $fecha;
+   
+   /**
+   * @ORM\Column(name="desccom", type="text", nullable=true)
+   */
+   private $desc;
+   
+   /**
+   * @var string
+   * @ORM\Column(name="nivelcom", type="string", length=255, nullable=true)
+   */
+   private $nivel;
+   
+   /**
+   * @var string
+   * @ORM\Column(name="federcom", type="string", length=255, nullable=true)
+   */
+   private $feder;
+   
+   /**
+   * @var string
+   * @ORM\Column(name="webcom", type="string", length=255, nullable=true)
+   */
+   private $web;
+   
+   /**
+   * @var string
+   * @ORM\Column(name="emailcom", type="string", length=255, nullable=true)
+   */
+   private $email;
+   
+   /**
+   * @var string
+   * @ORM\Column(name="cartelcom", type="string", length=255, nullable=true)
+   */
+   private $cartel;
+   
+   /**
+   * @var boolean
+   * @ORM\Column(name="esfedercom", type="boolean", options={"default":0})
+   */
+   private $esFeder = 0;
+   
+   /**
+   * @var boolean
+   * @ORM\Column(name="esoficialcom", type="boolean", options={"default":0})
+   */
+   private $esOficial = 0;
+   
+	public function getNombre() {
+		return $this->nombre;
+	}
+	public function setNombre($nombre) {
+		$this->nombre = $nombre;
+		return $this;
+	}
+	public function getTemp() {
+		return $this->temp;
+	}
+	public function setTemp($temp) {
+		$this->temp = $temp;
+		return $this;
+	}
+	public function getUbicacion() {
+		return $this->ubicacion;
+	}
+	public function setUbicacion($ubicacion) {
+		$this->ubicacion = $ubicacion;
+		return $this;
+	}
+	public function getSede() {
+		return $this->sede;
+	}
+	public function setSede($sede) {
+		$this->sede = $sede;
+		return $this;
+	}
+	public function getFecha() {
+		return $this->fecha;
+	}
+	public function setFecha($fecha) {
+		$this->fecha = $fecha;
+		return $this;
+	}
+	public function getDesc() {
+		return $this->desc;
+	}
+	public function setDesc($desc) {
+		$this->desc = $desc;
+		return $this;
+	}
+	public function getNivel() {
+		return $this->nivel;
+	}
+	public function setNivel($nivel) {
+		$this->nivel = $nivel;
+		return $this;
+	}
+	public function getFeder() {
+		return $this->feder;
+	}
+	public function setFeder($feder) {
+		$this->feder = $feder;
+		return $this;
+	}
+	public function getWeb() {
+		return $this->web;
+	}
+	public function setWeb($web) {
+		$this->web = $web;
+		return $this;
+	}
+	public function getEmail() {
+		return $this->email;
+	}
+	public function setEmail($email) {
+		$this->email = $email;
+		return $this;
+	}
+	public function getCartel() {
+		return $this->cartel;
+	}
+	public function setCartel($cartel) {
+		$this->cartel = $cartel;
+		return $this;
+	}
+	public function getEsFeder() {
+		return $this->esFeder;
+	}
+	public function setEsFeder($esFeder) {
+		$this->esFeder = $esFeder;
+		return $this;
+	}
+	public function getEsOficial() {
+		return $this->esOficial;
+	}
+	public function setEsOficial($esOficial) {
+		$this->esOficial = $esOficial;
+		return $this;
+	}
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="temporada", type="string", length=32)
-     */
-    private $temporada;
-
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="tipo", type="integer")
-     */
-    private $tipo;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="provincia", type="string", length=255)
-     */
-    private $provincia;
-
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="es_federada", type="boolean")
-     */
-    private $esFederada;
-
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="es_oficial", type="boolean")
-     */
-    private $esOficial;
-
-
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set nombre
-     *
-     * @param string $nombre
-     * @return Competicion
-     */
-    public function setNombre($nombre)
-    {
-        $this->nombre = $nombre;
-
-        return $this;
-    }
-
-    /**
-     * Get nombre
-     *
-     * @return string 
-     */
-    public function getNombre()
-    {
-        return $this->nombre;
-    }
-
-    /**
-     * Set temporada
-     *
-     * @param string $temporada
-     * @return Competicion
-     */
-    public function setTemporada($temporada)
-    {
-        $this->temporada = $temporada;
-
-        return $this;
-    }
-
-    /**
-     * Get temporada
-     *
-     * @return string 
-     */
-    public function getTemporada()
-    {
-        return $this->temporada;
-    }
-
-    /**
-     * Set tipo
-     *
-     * @param integer $tipo
-     * @return Competicion
-     */
-    public function setTipo($tipo)
-    {
-        $this->tipo = $tipo;
-
-        return $this;
-    }
-
-    /**
-     * Get tipo
-     *
-     * @return integer 
-     */
-    public function getTipo()
-    {
-        return $this->tipo;
-    }
-
-    /**
-     * Set provincia
-     *
-     * @param string $provincia
-     * @return Competicion
-     */
-    public function setProvincia($provincia)
-    {
-        $this->provincia = $provincia;
-
-        return $this;
-    }
-
-    /**
-     * Get provincia
-     *
-     * @return string 
-     */
-    public function getProvincia()
-    {
-        return $this->provincia;
-    }
-
-    /**
-     * Set esFederada
-     *
-     * @param boolean $esFederada
-     * @return Competicion
-     */
-    public function setEsFederada($esFederada)
-    {
-        $this->esFederada = $esFederada;
-
-        return $this;
-    }
-
-    /**
-     * Get esFederada
-     *
-     * @return boolean 
-     */
-    public function getEsFederada()
-    {
-        return $this->esFederada;
-    }
-
-    /**
-     * Set esOficial
-     *
-     * @param boolean $esOficial
-     * @return Competicion
-     */
-    public function setEsOficial($esOficial)
-    {
-        $this->esOficial = $esOficial;
-
-        return $this;
-    }
-
-    /**
-     * Get esOficial
-     *
-     * @return boolean 
-     */
-    public function getEsOficial()
-    {
-        return $this->esOficial;
-    }
+   
 }
