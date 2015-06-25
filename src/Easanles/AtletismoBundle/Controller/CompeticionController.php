3c84f5fcc3db6554abecd98885d604ac0353c5a8
Eduardo Alonso Sanlés <easanles@gmail.com>
2015-06-25 19:52:58 +0200

    Editar y borrar competiciones

--- src/Easanles/AtletismoBundle/Controller/CompeticionController.php
+++ src/Easanles/AtletismoBundle/Controller/CompeticionController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Easanles\AtletismoBundle\Entity\Competicion;
 use Symfony\Component\HttpFoundation\Request;
 use Easanles\AtletismoBundle\Form\Type\ComType;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class CompeticionController extends Controller
@@ -33,5 +34,45 @@ class CompeticionController extends Controller
     	 
        return $this->render('EasanlesAtletismoBundle:Competicion:formulario_competiciones.html.twig',
              array('form' => $form->createView()));
+    }
+   
+    public function borrarCompeticionAction(Request $request){
+    	 $nombre = $request->query->get('n');
+    	 $temp = $request->query->get('t');
+    	 
+    	 $em = $this->getDoctrine()->getManager();
+    	 $repository = $this->getDoctrine()->getRepository('EasanlesAtletismoBundle:Competicion');
+    	 $com = $repository->findOneBy(array('nombre' => $nombre, 'temp' => $temp));
+    	 $em->remove($com);
+    	 $em->flush();
+    	 return $this->redirect($this->generateUrl('listado_competiciones'));
+    }
+    
+    public function editarCompeticionAction(Request $request, $nombre, $temp){
+    	$em = $this->getDoctrine()->getManager();
+    	$repository = $this->getDoctrine()->getRepository('EasanlesAtletismoBundle:Competicion');
+    	$com = $repository->findOneBy(array('nombre' => $nombre, 'temp' => $temp));
+    	 
+    	$form = $this->createForm(new ComType(), $com);
+    	
+    	$form->handleRequest($request);
+    	
+    	if ($form->isValid()) {
+    		// guardar la tarea en la base de datos
+    		$em->flush();
+    		return $this->redirect($this->generateUrl('listado_competiciones'));
+    	}
+    	
+    	return $this->render('EasanlesAtletismoBundle:Competicion:formulario_competiciones.html.twig',
+    			array('form' => $form->createView()));
+    	
+    }
+    
+    public function verCompeticionAction($nombre, $temp){
+    	$repository = $this->getDoctrine()->getRepository('EasanlesAtletismoBundle:Competicion');
+    	$com = $repository->findOneBy(array('nombre' => $nombre, 'temp' => $temp));
+        	     	 
+    	return $this->render('EasanlesAtletismoBundle:Competicion:ver_competicion.html.twig',
+    			array('com' => $com));
     }
 }
--- web/bundles/atletismo/js/scripts.js
+++ web/bundles/atletismo/js/scripts.js
@@ -1,56 +1,5 @@
-/**
- * 
- */
-
-alerthtml_preok = "<div class=\"alert alert-info alert-dismissible fade in\" role=\"alert\"> <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button> <strong>Hecho: </strong><span>";
-alerthtml_preerr = "<div class=\"alert alert-error alert-dismissible fade in\" role=\"alert\"> <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button> <strong>Error: </strong><span>";
-alerthtml_pos = "</span></div>";
 
 
-function sendAction(path, icon){
-   $.get(path, function(data, status){
-	     if (status = "success"){
-			 $("#alert_div").append(alerthtml_preok + data + alerthtml_pos);
-	     } else {
-			 $("#alert_div").append(alerthtml_preerr + data + alerthtml_pos);
-	     }
-		 icon.removeClass("spinning");
-   });
+function goToUrl(path){
+	window.location.href = path;
 }
-
-$(document).ready(function(){
-	$(function () {
-		  $('[data-toggle="tooltip"]').tooltip()
-    })
-	
-	$("#btn_poblarbd").click(function(){
-	   icon = $(this).find("span");
-	   icon.addClass("spinning");
-   	   sendAction("./poblarbd", icon);
-	});
-
-    $("#btn_borrarbd").click(function(){
- 	   icon = $(this).find("span");
-	   icon.addClass("spinning");
-    	sendAction("./borrarbd", icon);
-    });
-	
-    $("#btn_rehacerbd").click(function(){
- 	   icon = $(this).find("span");
-	   icon.addClass("spinning");
-    	sendAction("./rehacerbd", icon);
-    });
-    
-    $("#btn_limpiarcache").click(function(){
- 	   icon = $(this).find("span");
-	   icon.addClass("spinning");
-    	sendAction("./limpiarcache", icon);
-    });
-    
-    $("#btn_asseticdump").click(function(){
- 	   //icon = $(this).find("span");
-	   //icon.addClass("spinning");
-	   //sendAction("./asseticdump", icon);
-    });
-
-});
