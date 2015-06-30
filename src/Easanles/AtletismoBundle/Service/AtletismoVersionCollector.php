7458b4afc4e9f2877fe17fd8dfe5613e0568cae8
Eduardo Alonso Sanlés <easanles@gmail.com>
2015-06-30 19:55:30 +0200

    Modificaciones plantillas competiciones

--- src/Easanles/AtletismoBundle/Service/AtletismoVersionCollector.php
+++ src/Easanles/AtletismoBundle/Service/AtletismoVersionCollector.php
@@ -13,7 +13,7 @@ class AtletismoVersionCollector extends DataCollector{
 	 * @see \Symfony\Component\HttpKernel\DataCollector\DataCollectorInterface::collect()
 	 */
 	public function collect(Request $request, Response $response, \Exception $exception = null) {
-		$VERSION = 'dev 12';
+		$VERSION = 'dev 13';
 		$ITERATION = '9';
 		
        $this->data = array(
