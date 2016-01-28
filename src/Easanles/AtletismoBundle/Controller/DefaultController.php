329a74a38e469db6b21019c3e98571874606d4d1
Eduardo Alonso Sanlés <easanles@gmail.com>
2016-01-28 20:33:01 +0100

    Control de acceso basico

--- src/Easanles/AtletismoBundle/Controller/DefaultController.php
+++ src/Easanles/AtletismoBundle/Controller/DefaultController.php
@@ -3,9 +3,29 @@
 namespace Easanles\AtletismoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller{
-    public function indexAction($name){
-        return $this->render('EasanlesAtletismoBundle:Default:index.html.twig', array('name' => $name));
+    public function indexAction($role, $name){
+        return $this->render('EasanlesAtletismoBundle:Default:index.html.twig', array('role' => $role, 'name' => $name));
     }
+    
+    
+    public function loginAction(Request $request){
+       $authenticationUtils = $this->get('security.authentication_utils');
+
+       $error = $authenticationUtils->getLastAuthenticationError();
+
+       $lastUsername = $authenticationUtils->getLastUsername();
+
+       return $this->render('EasanlesAtletismoBundle:Default:login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error)
+        );
+    }
+
+    public function loginCheckAction(){
+    	 
+    }
+    
 }
--- src/Easanles/AtletismoBundle/Service/AtletismoVersionCollector.php
+++ src/Easanles/AtletismoBundle/Service/AtletismoVersionCollector.php
@@ -13,7 +13,7 @@ class AtletismoVersionCollector extends DataCollector{
 	 * @see \Symfony\Component\HttpKernel\DataCollector\DataCollectorInterface::collect()
 	 */
 	public function collect(Request $request, Response $response, \Exception $exception = null) {
-		$VERSION = 'dev 96';
+		$VERSION = 'dev 97';
 		$ITERATION = '17 (de 21)';
 		
        $this->data = array(
