2cfdce122f6a5a0082a0374a8a58d6de6d56c1fe
Eduardo Alonso Sanlés <easanles@gmail.com>
2016-04-30 01:10:19 +0200

    Mostrar tipo atleta inscripcion cuotas

--- src/Easanles/AtletismoBundle/Service/AtletismoVersionCollector.php
+++ src/Easanles/AtletismoBundle/Service/AtletismoVersionCollector.php
@@ -13,7 +13,7 @@ class AtletismoVersionCollector extends DataCollector{
 	 * @see \Symfony\Component\HttpKernel\DataCollector\DataCollectorInterface::collect()
 	 */
 	public function collect(Request $request, Response $response, \Exception $exception = null) {
-		$VERSION = 'Beta v0.4b';
+		$VERSION = 'Beta v0.4c';
 		$ITERATION = '21 (de 21)';
 		
        $this->data = array(
